c35147c7327ed84ee41099daafcf237911db6e7f
Kunal Sambyal <kunalsambyal9628@gmail.com>
2026-09-07 22:57:47 +0530

    fix(typo): success in APIResponse

--- backend/app/schemas/response.py
+++ backend/app/schemas/response.py
@@ -5,7 +5,7 @@ T = TypeVar("T")
 
 
 class APIResponse(BaseModel, Generic[T]):
-    succsess: bool = True
+    success: bool = True
     code: int = 200
     message: str = "Operation successful"
     data: Optional[T] = None
